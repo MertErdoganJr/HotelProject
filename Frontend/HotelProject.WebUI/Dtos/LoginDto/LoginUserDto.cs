f701cd6fd8d661a5f2e4e8f0af8fe652f917a382
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-11-24 17:06:57 +0300

    Add Login and Register with Identity

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage ="Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Compare("Password", ErrorMessage = "Required")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
