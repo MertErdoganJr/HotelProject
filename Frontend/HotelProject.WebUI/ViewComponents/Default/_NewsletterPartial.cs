cb3961d29e7a92e4fc1ba12745dd8f2f5cbbb90f
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-11-27 19:07:06 +0300

    Add ViewComponent

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_NewsletterPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _NewsletterPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
