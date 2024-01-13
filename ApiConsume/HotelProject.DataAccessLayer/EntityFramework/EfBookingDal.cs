919794dcffacb84c4f712b315a57a1619cc936aa
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-01-13 21:34:21 +0300

    Add Booking Page

--- ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfBookingDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfBookingDal.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.EntityFramework
 {
-    internal class EfBookingDal : GenericRepository<Booking>, IBookingDal
+    public class EfBookingDal : GenericRepository<Booking>, IBookingDal
     {
         public EfBookingDal(Context context) : base(context)
         {
