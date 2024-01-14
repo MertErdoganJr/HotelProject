629f16d0358a5ccbc3eb0ff693f6971e28228874
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-01-14 03:16:36 +0300

    Add Booking Admin page

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,33 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:11834/api/Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
