7d4185cdc6b6950af5cd6c68247b54326d306ee7
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2024-02-01 21:40:20 +0300

    Add Count Trigger with SQL

--- ApiConsume/HotelProject.DataAccessLayer/Migrations/20240201174410_add_guest.Designer.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240201174410_add_guest.Designer.cs
@@ -4,14 +4,16 @@ using HotelProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240201174410_add_guest")]
+    partial class add_guest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
